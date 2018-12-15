582217777f77b1682cbc175d0888aa59540710ed
Ming Tsay <mt@moztw.org>
2018-12-15 13:44:33 +0800

    add .idea project and gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.10"
+}
+
+group = "tw.mingtsay"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("com.squareup.okhttp3", "okhttp", "3.12.0")
+    implementation("com.beust", "klaxon", "3.0.1")
+    testImplementation("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}

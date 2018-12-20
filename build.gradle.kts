49f66fd6409bc3773249408829a36bc1bd35b9a4
Ming Tsay <mt@moztw.org>
2018-12-20 10:29:09 +0800

    bump Kotlin from `1.3.10` to `1.3.11`

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.11"
 }
 
 group = "tw.mingtsay"
@@ -16,6 +16,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
+    implementation("commons-io", "commons-io", "2.6")
     implementation("com.squareup.okhttp3", "okhttp", "3.12.0")
     implementation("com.beust", "klaxon", "3.0.1")
     testImplementation("junit", "junit", "4.12")

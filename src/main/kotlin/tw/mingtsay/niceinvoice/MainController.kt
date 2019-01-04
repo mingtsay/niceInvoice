5f55e14db09a8bda6179627a4fca21abb94a5dac
Ming Tsay <mt@moztw.org>
2019-01-04 08:34:13 +0800

    implement input number on UI

--- src/main/kotlin/tw/mingtsay/niceinvoice/MainController.kt
+++ src/main/kotlin/tw/mingtsay/niceinvoice/MainController.kt
@@ -1,27 +1,28 @@
 package tw.mingtsay.niceinvoice
 
-import com.beust.klaxon.Klaxon
 import javafx.application.Platform
+import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
+import javafx.geometry.Insets
 import javafx.scene.control.*
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import javafx.scene.text.FontWeight
 import javafx.stage.Modality
 import javafx.stage.Stage
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import tw.mingtsay.niceinvoice.model.Invoice
+import tw.mingtsay.niceinvoice.model.InvoiceInputResult
+import tw.mingtsay.niceinvoice.model.InvoiceNumber
+import tw.mingtsay.niceinvoice.model.InvoiceResult
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class MainController {
-    private fun fetchInvoice() =
-        OkHttpClient().newCall(
-            Request.Builder()
-                .url("http://api.mingtsay.tw/invoice/")
-                .build()
-        ).execute().body()?.string()?.let { json -> Klaxon().parse<Invoice>(json) }
+    private var invoice = mutableListOf<InvoiceNumber>()
 
-    private var invoice: Invoice? = null
+    private var results = FXCollections.observableArrayList<InvoiceInputResult>()
 
     @FXML
     lateinit var menuBar: MenuBar
@@ -48,9 +49,30 @@ class MainController {
     lateinit var txtInput: TextField
 
     @FXML
+    lateinit var tableResults: TableView<InvoiceInputResult>
+
+    @FXML
+    lateinit var tableColumnNumber: TableColumn<InvoiceInputResult, String>
+
+    @FXML
+    lateinit var tableColumnDate: TableColumn<InvoiceInputResult, String>
+
+    @FXML
+    lateinit var tableColumnResult: TableColumn<InvoiceInputResult, String>
+
+    @FXML
+    lateinit var tableColumnPrice: TableColumn<InvoiceInputResult, String>
+
+    @FXML
     fun initialize() {
         menuBar.isUseSystemMenuBar = true
-        invoice = fetchInvoice()
+
+        tableColumnNumber.setCellValueFactory { it.value.numberProperty }
+        tableColumnDate.setCellValueFactory { it.value.invoiceTitleProperty }
+        tableColumnResult.setCellValueFactory { it.value.invoiceResultProperty }
+        tableColumnPrice.setCellValueFactory { it.value.invoicePriceProperty }
+        tableResults.items = results
+
         initializeInvoice()
     }
 
@@ -61,36 +83,34 @@ class MainController {
     }
 
     private fun initializeInvoice() {
-        val invoice = this.invoice
-        if (invoice != null) {
-            listInvoice.apply {
-                items.add(invoice.numbers[0].title)
-                items.add(invoice.numbers[1].title)
-                selectionModel.select(0)
-            }
-            applyInvoiceNumber(0)
-        } else {
-            Alert(Alert.AlertType.INFORMATION).apply {
-                headerText = "發票開獎資訊下載失敗"
-                dialogPane.content = Label("無法取得發票開獎資訊，請檢查您的網際網路連線。")
-                showAndWait()
-            }
-            Platform.exit()
+        if (invoice.isEmpty()) {
+            InvoiceNumber.fetch()
+                ?.apply { invoice = toMutableList() }
+                ?: Alert(Alert.AlertType.INFORMATION).apply {
+                    headerText = "發票開獎資訊下載失敗"
+                    dialogPane.content = Label("無法取得發票開獎資訊，請檢查您的網際網路連線。")
+                    showAndWait()
+                    Platform.exit()
+                }
         }
+
+        listInvoice.apply {
+            invoice.forEach { items.add(it.title) }
+            selectionModel.select(0)
+        }
+        applyInvoiceNumber(0)
     }
 
     private fun applyInvoiceNumber(index: Int) {
-        invoice?.apply {
-            with(numbers[index]) {
-                val formatter = DateTimeFormatter.ofPattern("Gyyy/M/d", Locale.TAIWAN)
+        invoice[index].apply {
+            val formatter = DateTimeFormatter.ofPattern("Gyyy/M/d", Locale.TAIWAN)
 
-                labelSuper.text = superNumber
-                labelSpecial.text = specialNumber
-                labelFirst.text = firstNumbers.joinToString("、")
-                labelAdditional.text = additionalNumbers.joinToString("、")
-                labelDate.text =
-                    String.format("自%s至%s止", date.dateFrom.format(formatter), date.dateTo.format(formatter))
-            }
+            labelSuper.text = superNumber
+            labelSpecial.text = specialNumber
+            labelFirst.text = firstNumbers.joinToString("、")
+            labelAdditional.text = additionalNumbers.joinToString("、")
+            labelDate.text =
+                String.format("自%s至%s止", date.dateFrom.format(formatter), date.dateTo.format(formatter))
         }
     }
 
@@ -111,10 +131,75 @@ class MainController {
 
     @FXML
     fun onInput() {
-        Alert(Alert.AlertType.INFORMATION).apply {
-            headerText = "Hello JavaFX"
-            dialogPane.content = ScrollPane(TextField(System.getProperty("os.name")).apply { isEditable = false })
-            showAndWait()
+        val number = txtInput.text
+
+        val errorMessage = when {
+            number.any { it !in "0123456789" } -> InputErrorMessage(title = "輸入格式錯誤", description = "您輸入的格式不正確！")
+            number.length !in arrayOf(3, 8) -> InputErrorMessage(title = "輸入長度錯誤", description = "您輸入的長度不正確！")
+            else -> null
         }
+
+        if (errorMessage != null) {
+            Alert(Alert.AlertType.ERROR).apply {
+                headerText = errorMessage.title
+                dialogPane.content = Label(
+                    "${errorMessage.description}\n" +
+                        "請輸入發票末三碼或完整號碼。"
+                )
+                showAndWait()
+            }
+            txtInput.selectAll()
+            return
+        } else {
+            val invoice = invoice[listInvoice.selectionModel.selectedIndex]
+            val result = invoice.compare(number)
+
+
+            when (result) {
+                InvoiceResult.INVOICE_RESULT_PERHAPS -> {
+                    Alert(Alert.AlertType.INFORMATION).apply {
+                        headerText = "可能中獎"
+                        dialogPane.content = Label(
+                            "您可能中獎了！\n" +
+                                "請輸入完整發票號碼來確認是否中獎。"
+                        )
+                        showAndWait()
+                    }
+                    txtInput.selectAll()
+                    return
+                }
+                InvoiceResult.INVOICE_RESULT_NONE -> {
+                }
+                else -> {
+                    Alert(Alert.AlertType.INFORMATION).apply {
+                        val margin = Insets(8.0)
+
+
+                        headerText = result.text
+                        dialogPane.content = VBox(
+                            Label("恭喜您中了${result.text}！").also { VBox.setMargin(it, margin) },
+                            Label(
+                                "您輸入的發票號碼 $number 對中了${result.text}，\n" +
+                                    "獎金為${result.price.text}。"
+                            ).also { VBox.setMargin(it, margin) },
+                            Label("請您妥善保存紙本發票以利領獎。").also { VBox.setMargin(it, margin) },
+                            Label(
+                                "歡迎使用發票載具保存電子發票，除了不用擔心發票不見以及自動化\n" +
+                                    "對獎等好處外，無實體電子發票還有專屬獎項，綁定銀行帳戶後還可將\n" +
+                                    "獎金直接匯入戶頭哦！"
+                            ).apply { textFill = Color.GRAY }.also { VBox.setMargin(it, margin) }
+                        ).apply { styleClass.add("font-sans-serif") }
+                        showAndWait()
+                    }
+                }
+            }
+
+            results.add(InvoiceInputResult(number = number, invoiceTitle = invoice.title, invoiceResult = result))
+        }
+
+        txtInput.text = ""
     }
 }
+
+//System.getProperty("os.name")
+private data class InputErrorMessage(val title: String, val description: String)
